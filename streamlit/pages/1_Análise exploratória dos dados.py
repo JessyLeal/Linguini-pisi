47bfec89b8e5e7af6cca1752099f2ef69c0863ac
Jorge Lucas <jorge.lucasalm@gmail.com>
2022-09-21 03:43:05 -0300

    feat: adiciona seleção de colunas, tratamento de atributos categóricos e escalonamento de valores

--- streamlit/pages/1_Análise exploratória dos dados.py
+++ streamlit/pages/1_Análise exploratória dos dados.py
@@ -1,6 +1,9 @@
-import os, io
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from nltk.corpus import stopwords
+import nltk
+import os
+import io
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 
 import seaborn as sns  # visualization tool
 import matplotlib
@@ -9,7 +12,7 @@ import missingno as msno
 import streamlit as st
 
 from sklearn.impute import SimpleImputer
-import plotly.express as px 
+import plotly.express as px
 
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
@@ -19,8 +22,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 
-import nltk
-from nltk.corpus import stopwords
 nltk.download('stopwords')
 nltk.download('punkt')
 
@@ -28,14 +29,14 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 st.title('Food Recipes Dataset Exploratory Data Analysis')
 
 tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(
-  [
-    "Exploração dos dados", 
-    "Limpeza dos dados",
-    "Seleção de colunas", 
-    "Tratamento de atributos categóricos", 
-    "Escalonamento de valores", 
-    "Visualização e Análise"
-  ]
+    [
+        "Exploração dos dados",
+        "Limpeza dos dados",
+        "Seleção de colunas",
+        "Tratamento de atributos categóricos",
+        "Escalonamento de valores",
+        "Visualização e Análise"
+    ]
 )
 with tab1:
     st.header('1. Exploração dos dados')
@@ -80,13 +81,14 @@ with tab1:
     st.line_chart((df.isna().sum()))
 
     col1, col2 = st.columns(2)
-    with col1: 
-        graph = sns.countplot(x = df['record_health']); #this column must be dropped
+    with col1:
+        # this column must be dropped
+        graph = sns.countplot(x=df['record_health'])
         st.text(graph)
-        st.pyplot();
+        st.pyplot()
     with col2:
-        h = sns.histplot(x = 'diet',data = df, bins = 20);
-        plt.xticks(rotation=90);
+        h = sns.histplot(x='diet', data=df, bins=20)
+        plt.xticks(rotation=90)
         plt.tight_layout()
         st.text(h)
         st.pyplot()
@@ -95,14 +97,15 @@ with tab2:
     st.subheader("2.1. Visualização dos dados")
     matrix = msno.matrix(df)
     st.text(matrix)
-    st.pyplot();
+    st.pyplot()
 
     st.dataframe(df.head(3))
 
     cols_with_missing = [col for col in df.columns if df[col].isnull().any()]
     st.subheader("2.2. Exclusão de linhas e colunas")
     st.write("Excluir receitas duplicatas e linhas com valores nulos nas colunas de ingredientes, instruções, título de receita e dieta.")
-    df = df.dropna(subset=['ingredients','instructions', 'recipe_title', 'diet'])
+    df = df.dropna(
+        subset=['ingredients', 'instructions', 'recipe_title', 'diet'])
     df = df.drop(['url', 'record_health'], axis=1)
     st.text(df.columns)
     st.dataframe(df.head(3))
@@ -115,15 +118,76 @@ with tab2:
     ''')
 
     df['ingredients'] = df['ingredients'].apply(lambda x: x.split('|'))
-    df['instructions'] = df['instructions'].apply(lambda x: x.replace('|',''))
-    df['tags'] = df['tags'].apply(lambda x: x.split('|') if type(x)!= float else x)
-    df['prep_time']=df['prep_time'].apply(lambda x: int(x.replace('M', '')) if type(x)!= float else x)
-    df['cook_time'] = df['cook_time'].apply(lambda x: int(x.replace('M', '')) if type(x)!= float else x)
+    df['instructions'] = df['instructions'].apply(lambda x: x.replace('|', ''))
+    df['tags'] = df['tags'].apply(
+        lambda x: x.split('|') if type(x) != float else x)
+    df['prep_time'] = df['prep_time'].apply(lambda x: int(
+        x.replace('M', '')) if type(x) != float else x)
+    df['cook_time'] = df['cook_time'].apply(lambda x: int(
+        x.replace('M', '')) if type(x) != float else x)
 
-    st.dataframe(df[['ingredients', 'instructions', 'tags', 'prep_time', 'cook_time']])
+    st.dataframe(
+        df[['ingredients', 'instructions', 'tags', 'prep_time', 'cook_time']])
     st.subheader("2.4. Tratamento de valores ausentes")
 
-    df['cook_time'].fillna(df['cook_time'].mean(), inplace = True)
-    df['prep_time'].fillna(df['prep_time'].mean(), inplace = True)
+    df['cook_time'].fillna(df['cook_time'].mean(), inplace=True)
+    df['prep_time'].fillna(df['prep_time'].mean(), inplace=True)
 
     st.line_chart((df.isna().sum()))
+
+with tab3:
+    st.header('3. Seleção de colunas')
+    st.write("""A seleção de dados para o treinamento do nosso modelo é uma etapa crucial no desenvolvimento do modelo de Machine Learning.
+    
+    Dessa forma, as colunas selecionadas para treinamento e validação de modelo serão: 
+    1. vote_count: Variável numérica discreta, contabiliza a quantidade de votos de uma receita.
+    2. rating: Variável numérica contínua, informa a avaliação da receita pelo público. 
+    3. description: Variável categórica nominal, se trata de uma breve descrição da receita. 
+    4. cuisine: Variável categórica nominal, informa a cozinha regional da receita. 
+    5. course: Variável categórica nominal, informa o tipo do prato. 
+    6. diet: Variável categórica nominal, informa qual dieta engloba a receita. 
+    7. prep_time: Variável numérica contínua, informa o tempo de preparação da receita. 
+    8. cook_time: Variável numérica contínua, informa o tempo de cozimento da receita.
+    9. ingredients: Variável categórica nominal, informa os ingredientes presentes na receita, mas não informa quantidade. 
+    10. instructions: Variável categórica nominal, armazena a instrução de preparo da receita. 
+    12. category: Variável categórica nominal, armazena a categoria da receita. """)
+
+    x = df.iloc[:, [1, 2, 3, 4, 5, 6, 7, 8, 13]]
+    st.dataframe(x.head(3))
+
+with tab4:
+    st.header('4. Tratamento de atributos categóricos')
+    st.write(
+        """O modelo de Machine Learning somente trabalha com valores numéricos e não com strings. Dessa forma, será utilizado o módulo [OneHotEncoder](https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.OneHotEncoder.html) disponibilizado pela biblioteca [Sklearn](https://scikit-learn.org/stable/).""")
+    st.write("""As colunas que terão seus atributos categóricos tratados são: """)
+    st.write("""
+    1. cuisine
+    2. description
+    3. course
+    4. diet
+    5. category
+    """)
+    st.write("O tratamendo da coluna 'description' será através do módulo TF-IDF Vectorizar da biblioteca Sklearn a fim de converter-la num formato numérico. O termo TF significa frequência do termo, enquanto IDF significa frequência de documento inversa. Dessa forma, o valor do TF-IDF aumenta de forma proporcional a frequência que aquele termo aparece em um determinado número de textos.")
+    vectorizer = TfidfVectorizer(sublinear_tf=True, stop_words=stopwords.words(
+        'portuguese'), min_df=5, max_df=0.95)
+
+    onehotencoder = ColumnTransformer(transformers=[('', OneHotEncoder(
+    ), ['cuisine', 'course', 'diet', 'category'])], remainder='passthrough')
+    x_new = onehotencoder.fit_transform(
+        x[['cuisine', 'course', 'diet', 'category']]).toarray()
+    st.dataframe(x_new[1:6, :])
+
+    x_description = x['description'].values.astype('U')
+    vectorizer = TfidfVectorizer(sublinear_tf=True, stop_words=stopwords.words(
+        'portuguese'), min_df=5, max_df=0.95)
+    x_description = vectorizer.fit_transform(x_description)
+    x_description = x_description.toarray()
+
+
+with tab5:
+    st.header('5. Escalonamento de valores')
+    st.write(
+        """A fim de normalizar os valores numéricos existentes, será aplicado uma função de normalização disponibilizado pela biblioteca [Sklearn](https://scikit-learn.org/stable/) chamada **Standard Scaler**.""")
+
+    x_new = scaler.fit_transform(x_new)
+    st.dataframe(x_new[0:5, :])
